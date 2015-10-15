5f916c81ca07e8c6e2f223f0f723d804aa2cf714
Sebastiano Mariani <mariani.sebastiano@gmail.com>
2015-10-15 05:47:18 -0700

    added some comments
    Signed-off-by: Sebastiano Mariani <mariani.sebastiano@gmail.com>

--- WriteInterval.h
+++ WriteInterval.h
@@ -6,12 +6,16 @@ class WriteInterval
 {
 
 public:
-
+	//create a new WriteInterval 
 	WriteInterval(ADDRINT addr_begin, ADDRINT addr_end);
 	~WriteInterval(void);
+	//check if we have to expand our interval
 	BOOL checkUpdate(ADDRINT start_addr, ADDRINT end_addr);
+	//check if a given address is inside our interval
 	BOOL checkInside(ADDRINT ip);	
+	//update our inteval with the new bounds
 	VOID update(ADDRINT start_addr, ADDRINT end_addr);
+	//getter
 	ADDRINT getAddrBegin();
 	ADDRINT getAddrEnd();
 
