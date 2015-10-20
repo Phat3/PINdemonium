0d985c25411ee34dd00780d7f3fef21f44764785
Sebastiano Mariani <mariani.sebastiano@gmail.com>
2015-10-20 05:44:09 -0700

    added some comments in the long jmp heuristic
    Signed-off-by: Sebastiano Mariani <mariani.sebastiano@gmail.com>

---
+++ EntropyHeuristic.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "Heuristics.h"
+
+
+class EntropyHeuristic
+{
+public:
+	UINT32 run();
+};
--- LongJumpHeuristic.cpp
+++ LongJumpHeuristic.cpp
@@ -1,23 +1,23 @@
 #include "LongJumpHeuristic.h"
 
-#define JMP_THRESHOLD 0x200
+//specify the range of the jump when it is considered a long jump or not
+const ADDRINT JMP_THRESHOLD =  0x200;
 
-UINT32 LongJumpHeuristic::run(INS ins , ADDRINT prev_ip){
-
+UINT32 LongJumpHeuristic::run(INS ins, ADDRINT prev_ip){
+	//filter out the improper values 
 	if(prev_ip > 0){
-
-		MYLOG("CHIAMATA EURISTICA 4!!");
+		//get the current IP
 		ADDRINT ip = INS_Address(ins);
 
 		ADDRINT diff;
-
+		//calculate the difference (we have to implement a modulo for the unsigned hex, this is only a placeholder)
 		if(prev_ip > ip){
 			diff = prev_ip - ip;
 		}
 		else{
 			diff = ip - prev_ip;
 		}
-		
+		//if the difference from the IP of the jump to the target of the jmp is greater than our threshold then a long jmp i sdetected
 		if(diff > JMP_THRESHOLD){
 			MYLOG("[LONG JMP DETECTED!!] FROM : %08x	TO : %08x", prev_ip, ip);
 			MYLOG("");
