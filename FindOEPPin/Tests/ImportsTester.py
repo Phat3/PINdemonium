4cf9462dda248d16508146d1000655fde1f43db7
Stefano <stefano496@gmail.com>
2016-02-16 16:41:01 +0100

    Modified ImportsTester.py script
    Signed-off-by: Stefano <stefano496@gmail.com>

--- FindOEPPin/Tests/ImportsTester.py
+++ FindOEPPin/Tests/ImportsTester.py
@@ -9,34 +9,35 @@ import json
 malware_folder = "C:\\Users\\phate\\Desktop\\Malware Results\\"
 
 def getCurrentMalware():
-  #get the list of malwares to analize
+  #get the list of folders of results to analize
   malwares_results = [f for f in listdir(malware_folder)]
   length = len(malwares_results)
   if length == 0:
     print("Malware Results folder empty")
     return None
-  print("Current malwares "+str(malwares_results))
-  #move the malware to the work folder
   for i in range(0,length):
-    partial_path = join(malware_folder,malwares_results[i])  
+    #path to a single folder of a malware result
+    partial_path = join(malware_folder,malwares_results[i])
+    #path to the report file of a malware result
     complete_path = join(partial_path,"report_FindOEPPin.txt")
-    report_file = open(complete_path,'r')
-    print "\n-------------MALWARE " + str(malwares_results[i]) + "-------------\n"
-    index_best_dump = -1
-    functions_detected_best = -1
-    for line in report_file:
-        new_dict = dict(json.loads(line).items())
-        print "Parsing dump number " + str(new_dict['dump number'])
-        if new_dict['runnable?'] == 'PROBABLY YES':
-                functions_detected = new_dict['detected_functions'].split("/")[0]                                               #count the number of detected import funtions
-                print "-> number of malicious imported functions detected: " + str(functions_detected)
-                if functions_detected > functions_detected_best:
-                        functions_detected_best = functions_detected
-                        index_best_dump = new_dict['dump number']
-                i = i + 1
-        else:
-                print "-> dump not runnable"
-        print "-------------------------------------------------------"
+    if isfile(complete_path):
+      report_file = open(complete_path,'r')
+      print "\n-------------MALWARE " + str(malwares_results[i]) + "-------------\n"
+      index_best_dump = -1
+      functions_detected_best = -1
+      for line in report_file:
+          new_dict = dict(json.loads(line).items())
+          print "Parsing dump number " + str(new_dict['dump number'])
+          if new_dict['runnable?'] == 'PROBABLY YES':
+                  functions_detected = new_dict['detected_functions'].split("/")[0]                                               #count the number of detected import funtions
+                  print "-> number of malicious imported functions detected: " + str(functions_detected)
+                  if functions_detected > functions_detected_best:
+                          functions_detected_best = functions_detected
+                          index_best_dump = new_dict['dump number']
+                  i = i + 1
+          else:
+                  print "-> dump not runnable"
+          print "-------------------------------------------------------"
  
 def main():
   getCurrentMalware()
