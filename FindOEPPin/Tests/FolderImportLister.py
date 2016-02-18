b73102e95ab606ee47c327d0478104d0a5a1b7b8
Stefano <stefano496@gmail.com>
2016-02-18 16:01:47 +0100

    Modified tests scripts
    Signed-off-by: Stefano <stefano496@gmail.com>

--- FindOEPPin/Tests/FolderImportLister.py
+++ FindOEPPin/Tests/FolderImportLister.py
@@ -3,8 +3,8 @@ from os import listdir
 from os.path import isfile, join
 
 
-ida_path = "C:\Program Files\IDA 6.6\idaw.exe"
-importLister_script = "C:\pin\\tests\importLister.py"
+ida_path = "C:\\Program Files\\IDA 6.6\\idaw.exe"
+importLister_script = "C:\\Users\\phate\\Desktop\\FindOEPPin\\FindOEPPin\\Tests\\importLister.py"
 
 
 def generateImportsFile(mypath):
@@ -12,10 +12,11 @@ def generateImportsFile(mypath):
   for cur_file in files:
     if cur_file.split(".")[-1] == "exe":
       out_file = "".join(cur_file.split(".")[:-1]) + "_imports.txt"
-      importLister_command = "\"" + ida_path + "\"" + " -A -S"+"\""+importLister_script + " " + out_file  + "\""+ " " + "\"" + cur_file + "\""
+      importLister_command = "\"" + ida_path + "\"" + " -A -S"+"\""+importLister_script + " " + join(mypath, out_file)  + "\""+ " " + "\"" + join(mypath, cur_file) + "\""
       print importLister_command
       try:
         check_output(importLister_command,shell=True)
       except Exception as e:
         print "error "+str(e)+" processing "+importLister_command
-generateImportsFile("C:\pin\PinUnpackerResults\\2016_02_16_06_41_31\\")
+        
+#generateImportsFile("C:\\Users\\phate\\Desktop\\be55f5db33ad80e12683b238872bb78bee2975270773069270cf1926a7de1d99\\")
--- FindOEPPin/Tests/ImportsTester.py
+++ FindOEPPin/Tests/ImportsTester.py
@@ -5,6 +5,8 @@ import time
 import sys
 import shutil
 import json
+import FolderImportLister
+from FolderImportLister import generateImportsFile
 
 malware_folder = "C:\\Users\\phate\\Desktop\\Malware Results\\"
 
@@ -15,13 +17,28 @@ def getCurrentMalware():
   if length == 0:
     print("Malware Results folder empty")
     return None
+  
   for i in range(0,length):
     #path to a single folder of a malware result
-    partial_path = join(malware_folder,malwares_results[i])
-    #path to the report file of a malware result
-    complete_path = join(partial_path,"report_FindOEPPin.txt")
-    if isfile(complete_path):
-      report_file = open(complete_path,'r')
+    folder_path = join(malware_folder,malwares_results[i])
+    generateImportsFile(folder_path)
+    #path to the original imports file of a malware result folder
+    original_imports_path = join(folder_path,"original_imports.txt")
+    if isfile(original_imports_path):
+      original_imports_file = open(original_imports_path, "r")
+      #original_imports = original_imports_file.readlines()
+      #print original_imports
+                        
+ 
+def main():
+  getCurrentMalware()
+
+main()
+
+  
+  
+
+"""report_file = open(complete_path,'r')
       print "\n-------------MALWARE " + str(malwares_results[i]) + "-------------\n"
       index_best_dump = -1
       functions_detected_best = -1
@@ -37,12 +54,4 @@ def getCurrentMalware():
                   i = i + 1
           else:
                   print "-> dump not runnable"
-          print "-------------------------------------------------------"
- 
-def main():
-  getCurrentMalware()
-
-main()
-
-
-  
+          print "-------------------------------------------------------" """
