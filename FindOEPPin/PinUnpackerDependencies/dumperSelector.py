3ba561d3b7f48cc8030b9a8510aaa5b10cb616e8
Stefano <stefano496@gmail.com>
2016-03-16 17:53:35 +0100

    Corrected bug in ImportsTester.py and dumperSelector.py
    Signed-off-by: Stefano <stefano496@gmail.com>

--- FindOEPPin/PinUnpackerDependencies/dumperSelector.py
+++ FindOEPPin/PinUnpackerDependencies/dumperSelector.py
@@ -11,8 +11,8 @@ for line in report_file:
         new_dict = dict(json.loads(line).items())
         print "Parsing dump number " + str(new_dict['dump number'])
         if new_dict['runnable?'] == 'PROBABLY YES':
-                zero_count = len( dict((key, val) for key, val in json.loads(line).items() if all(x == '0' for x in val)) )	#filter all the val that is not 0 and get the count of the resulting dict
-                functions_detected = new_dict['detected_functions'].split("/")[0]                                               #count the number of detected import funtions
+                zero_count = len( dict((key, val) for key, val in json.loads(line).items() if (all(x == '0' for x in val) and key != 'dump number')) )	#filter all the val that is not 0 and get the count of the resulting dict
+                functions_detected = new_dict['detected_functions'].split("/")[0]                                                                       #count the number of detected import funtions
                 print "-> zero count: " + str(zero_count)
                 print "-> number of malicious imported functions detected: " + str(functions_detected)
                 if functions_detected > functions_detected_best and zero_count > zero_count_best:
