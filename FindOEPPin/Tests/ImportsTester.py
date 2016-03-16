c71fcb46197b482a5289a883e611668b0bbb2123
Stefano <stefano496@gmail.com>
2016-03-16 16:56:21 +0100

    ImportTester.py now counts the zeros
    Signed-off-by: Stefano <stefano496@gmail.com>

--- FindOEPPin/Tests/ImportsTester.py
+++ FindOEPPin/Tests/ImportsTester.py
@@ -21,6 +21,7 @@ from FolderImportLister import generateImportsFile
 
 malware_folder = "E:\\Results"
 result_file_name = "results.txt"
+final_report_file_name = "report_FindOEPPin.txt"
 
 def analyseTests():
   #get the list of folders of results to analize
@@ -40,6 +41,7 @@ def analyseTests():
     original_imports_path = join(folder_path,"original_imports.txt")
     best_dump = -1
     best_number_new_imports = -1
+    best_zero_count = -1
 
     if isfile(original_imports_path):
       #rename the malware sample to its SHA1
@@ -48,9 +50,16 @@ def analyseTests():
       #get the original imports of the malware
       original_imports_file = open(original_imports_path, "r")
       original_imports = original_imports_file.readlines()
+      #get the report file
+      final_report_file_path = join(folder_path, final_report_file_name)
+      final_report_file = open(final_report_file_path, "r")
+      final_report_lines = final_report_file.readlines()
+      dump_number = -1
       #get the imports of each dump
       for cur_file in listdir(folder_path):
         splitted_name = cur_file.split("_")
+        if len(splitted_name) == 3:
+          dump_number = cur_file.split("_")[1]
         if any("imports.txt" in s for s in splitted_name) and not(any("original" in s for s in splitted_name)):
           dump_import_path = join(folder_path, cur_file)
           dump_imports_file = open(dump_import_path, "r")
@@ -58,16 +67,26 @@ def analyseTests():
           for f in dump_imports:
             if f == "?\n":
               dump_imports.remove(f)
-          #compare the dump imports with the original ones and eventually update the best results
+          #compare the dump imports with the original ones and the zero count with the previously saved value and eventually update the best results
+          print final_report_lines[int(dump_number)]
+          #final_report_fields = final_report_lines[int(dump_number)].split(",");
+          #zero_count = 0
+          #for f in final_report_fields:
+          #  if f.split(":")[1] == '"0"':
+          #    zero_count += 1
+          #  print f.split(":")[1]
+          #print "\n" + str(zero_count)
+          zero_count = len( dict((key, val) for key, val in json.loads(final_report_lines[int(dump_number)]).items() if all(x == '0' for x in val)) )
           new_dump_imports = list(set(dump_imports) - set(original_imports))
-          if (len(new_dump_imports) > best_number_new_imports):
+          if (len(new_dump_imports) > best_number_new_imports and zero_count > best_zero_count):
+            best_zero_count = zero_count
             best_dump = cur_file
             best_number_new_imports = len(new_dump_imports)
 
     #save the results for the current malware
     result_file_path = malware_folder + "\\" + malwares_results[i] + "\\" + result_file_name
     result_file = open(result_file_path, "a")
-    result_file.write("Malware: " + str(malwares_results[i]) + ", best_number_new_imports: " + str(best_number_new_imports) + ", best dump: " + str(best_dump) + "\n")
+    result_file.write("Malware: " + str(malwares_results[i]) + ", best_number_new_imports: " + str(best_number_new_imports) + ", best zero count: " + str(best_zero_count) + ", best dump: " + str(best_dump) + "\n")
           
 def main():
   analyseTests()
