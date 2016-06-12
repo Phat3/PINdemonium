5da79079ad40f4c4ab025a586ee8e4069935ae0e
Sebastiano Mariani <mariani.sebastiano@gmail.com>
2016-06-12 18:30:33 +0200

    used a dirty trick in order to get coordibnates and bounds of shape with getchildByName method
    Signed-off-by: Sebastiano Mariani <mariani.sebastiano@gmail.com>

--- PINdemoniumReport/app/src/MemoryLayout/memoryLayout.jsx
+++ PINdemoniumReport/app/src/MemoryLayout/memoryLayout.jsx
@@ -8,6 +8,7 @@ class MemoryLayout extends React.Component {
     this._setHeight = this._setHeight.bind(this)
     this._drawMemory = this._drawMemory.bind(this)
     this._drawDump = this._drawDump.bind(this)
+    this._drawArrow = this._drawArrow.bind(this)
   }
 
   //set the proper dimensions for the canvas object based on the viewport dimension
@@ -23,28 +24,28 @@ class MemoryLayout extends React.Component {
   _drawMemory(){
     // draw the rectangle representing the memory of the process
     var memorySpace = new createjs.Shape();
-    var memorySpaceWidth = this.canvas.width / 3
-    var memorySpaceHeight = this.canvas.height - 50 
-    memorySpace.graphics.beginFill("DeepSkyBlue").drawRect(memorySpaceWidth, 50, memorySpaceWidth, memorySpaceHeight);
-    this.stage.addChild(memorySpace);
+    memorySpace.width = this.canvas.width / 3
+    memorySpace.height = this.canvas.height - 50 
+    memorySpace.x = memorySpace.width
+    memorySpace.y = 50
+    memorySpace.graphics.beginFill("DeepSkyBlue").drawRect(0, 0, memorySpace.width, memorySpace.height);
 
     // draw the label above the rectangle representing the process
     var labelMemorySpace = new createjs.Text("Memory Layout", "30px Arial", "black");
     // center the label on the rectangle
     var labelMemorySpaceBounds = labelMemorySpace.getBounds()
-    labelMemorySpace.x = memorySpaceWidth + ( (memorySpaceWidth - labelMemorySpaceBounds.width) / 2)
-    this.stage.addChild(labelMemorySpace);
+    labelMemorySpace.x = memorySpace.width + ( (memorySpace.width - labelMemorySpaceBounds.width) / 2)
 
     // draw the addresses label on the right of the memory layout
     var labelMemoryFirstAddress = new createjs.Text("0x00", "20px Arial", "red");
     var labelMemoryLastAddress = new createjs.Text("0xff", "20px Arial", "red");
-    var labelmemoryAddressX = memorySpaceWidth * 2 + 10
+    var labelmemoryAddressX = memorySpace.width * 2 + 10
     labelMemoryFirstAddress.x = labelmemoryAddressX
     labelMemoryFirstAddress.y = 50
     labelMemoryLastAddress.x = labelmemoryAddressX
-    labelMemoryLastAddress.y = memorySpaceHeight - (labelMemoryLastAddress.getBounds().height) + 50
-    this.stage.addChild(labelMemoryFirstAddress);
-    this.stage.addChild(labelMemoryLastAddress);
+    labelMemoryLastAddress.y = memorySpace.height - (labelMemoryLastAddress.getBounds().height) + 50
+
+    this.stage.addChild(memorySpace, labelMemorySpace, labelMemoryFirstAddress, labelMemoryLastAddress);
     // update the canvas
     this.stage.update();
   }
@@ -52,34 +53,58 @@ class MemoryLayout extends React.Component {
 
   _drawDump(){
     // draw the rectangle representing the memory of the process
-    var dumpShape = new createjs.Shape();
-    var dumpShapeWidth = this.canvas.width / 2.5
-    var dumpShapeHeight = 100
-    var dumpShapeX = (this.canvas.width - dumpShapeWidth)/2
-    var dumpShapeY = 300
-    dumpShape.graphics.beginFill("red").drawRect(dumpShapeX, dumpShapeY, dumpShapeWidth, dumpShapeHeight);
-    this.stage.addChild(dumpShape);
+    var dumpShape = new createjs.Shape()
+    dumpShape.name = "prova"
+    dumpShape.width = this.canvas.width / 2.5
+    dumpShape.height = 100
+    dumpShape.x = (this.canvas.width - dumpShape.width)/2
+    dumpShape.y = 200
+    dumpShape.graphics.beginFill("red").drawRect(0, 0, dumpShape.width, dumpShape.height);
     
     // draw the label above the rectangle representing the process
     var labelDumpShape = new createjs.Text("DUMP 1", "30px Arial", "white");
     // center the label on the rectangle
     var labelDumpShapeBounds = labelDumpShape.getBounds()
-    labelDumpShape.x = dumpShapeX + ( (dumpShapeWidth - labelDumpShapeBounds.width) / 2)
-    labelDumpShape.y = dumpShapeY + ( (dumpShapeHeight - labelDumpShapeBounds.height) / 2)
-    this.stage.addChild(labelDumpShape);
+    labelDumpShape.x = dumpShape.x + ( (dumpShape.width - labelDumpShapeBounds.width) / 2)
+    labelDumpShape.y = dumpShape.y + ( ( dumpShape.height - labelDumpShapeBounds.height) / 2)
     
     // draw the addresses label on the right of the memory layout
     var labelDumpShapeFirstAddress = new createjs.Text("0x00400040", "20px Arial", "blue");
     var labelDumpShapeLastAddress = new createjs.Text("0x00411040", "20px Arial", "blue");
-    labelDumpShapeFirstAddress.x = dumpShapeWidth + dumpShapeX + 10
-    labelDumpShapeFirstAddress.y = dumpShapeY
-    labelDumpShapeLastAddress.x = dumpShapeWidth + dumpShapeX + 10
-    labelDumpShapeLastAddress.y = dumpShapeY +  dumpShapeHeight - (labelDumpShapeLastAddress.getBounds().height)
-    this.stage.addChild(labelDumpShapeFirstAddress);
-    this.stage.addChild(labelDumpShapeLastAddress);
+    labelDumpShapeFirstAddress.x = dumpShape.width + dumpShape.x + 10
+    labelDumpShapeFirstAddress.y = dumpShape.y
+    labelDumpShapeLastAddress.x = dumpShape.width + dumpShape.x + 10
+    labelDumpShapeLastAddress.y = dumpShape.y +   dumpShape.height - (labelDumpShapeLastAddress.getBounds().height)
+
+    this.stage.addChild(dumpShape, labelDumpShape, labelDumpShapeFirstAddress, labelDumpShapeLastAddress);
     // update the canvas
-    
     this.stage.update();
+  }
+
+  _drawArrow(){
+
+    var drawingCanvas = new createjs.Shape();
+    this.stage.addChild(drawingCanvas);
+
+    drawingCanvas.graphics.setStrokeStyle(2).beginStroke(createjs.Graphics.getRGB(0, 0, 0)).moveTo(10, 10).lineTo(200,200);
+
+    drawingCanvas.graphics.setStrokeStyle(2).beginStroke(createjs.Graphics.getRGB(0, 0, 0)).moveTo(200, 200).lineTo(200,300);
+
+
+    var radian = Math.atan2((110 - 240), (90 - 90))
+
+
+    var arrow = new createjs.Shape();
+    arrow.graphics.beginStroke(createjs.Graphics.getRGB(0, 0, 0)).moveTo(-5, +5).lineTo(0, 0).lineTo(-5, -5);
+    
+    var degree = radian / Math.PI * 180;
+    arrow.x = 100;
+    arrow.y = 100;
+    arrow.rotation = degree;
+
+    this.stage.addChild(arrow);
+    this.stage.update();
+
   }
 
   //callback of reactjs called when th component mounting is finished
@@ -89,6 +114,8 @@ class MemoryLayout extends React.Component {
     this.stage = new createjs.Stage("memoryLayoutCanvas");
     this._drawMemory()
     this._drawDump()
+    this._drawArrow()
+    //var prev_dump = this.stage.getChildByName("prova")
   }
 
   render () {
@@ -98,7 +125,7 @@ class MemoryLayout extends React.Component {
         <canvas id="memoryLayoutCanvas"></canvas>
       </div>
     );
-    
+
   }
 
 }
