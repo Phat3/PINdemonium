aa911f0a00d0ced8f1723f7706226c46105d079a
Ugo <lord.fontana@gmail.com>
2016-07-02 01:25:20 -0700

    solved bug space inside exe name
    Signed-off-by: Ugo <lord.fontana@gmail.com>

--- PINdemonium/Helper.cpp
+++ PINdemonium/Helper.cpp
@@ -29,3 +29,13 @@ vector<string> Helper::split(const string &s, char delim) {
     }
     return elems;
 }
+
+
+string Helper::replace_string(string str, const string& from, const string& to) {
+    size_t start_pos = 0;
+    while((start_pos = str.find(from, start_pos)) != std::string::npos) {
+        str.replace(start_pos, from.length(), to);
+        start_pos += to.length(); // Handles case where 'to' is a substring of 'from'
+    }
+    return str;
+}
--- PINdemonium/Helper.h
+++ PINdemonium/Helper.h
@@ -7,5 +7,6 @@ public:
 	Helper(void);
 	static BOOL existFile (string name);
 	static 	vector<string> split(const string &s, char delim);
+	static string replace_string(string str, const string& from, const string& to) ;
 };
 
